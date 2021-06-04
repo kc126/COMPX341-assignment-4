58a9caf65e08d214c4bfdf8a2603e5b6f9fdca40
kc126 <kc126@students.waikato.ac.nz>
2021-06-04 19:33:19 +1200

    Task 1 - inital code and run - completed

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -2,6 +2,13 @@
 # Run and build the code
 npm install
 npm run build
+if [ $? -eq 0 ]
+then
+  echo "SUCCESS BUILD"
+else
+  echo "FAIL"
+  exit 2
+fi
 # Commit code
 git add .
 git commit -m "Task 1 - inital code and run - completed"
