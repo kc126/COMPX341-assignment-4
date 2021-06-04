451e0d673e256ea30e74add855d90ef385546ad1
kc126 <kc126@students.waikato.ac.nz>
2021-06-04 20:52:12 +1200

    Task 2 - part 2 completed and tested - uncommented code

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -6,19 +6,19 @@ if [ $(python documentationCheck.py) = 0 ]; then
 fi
 echo "Files have the correct documentation"
 # Run and build the code
-# npm install
-# npm run build
-# if [ $? -eq 0 ]
-# then
-#   echo "SUCCESS BUILD"
-# else
-#   echo "FAIL"
-#   exit 2
-# fi
+npm install
+npm run build
+if [ $? -eq 0 ]
+then
+  echo "SUCCESS BUILD"
+else
+  echo "FAIL"
+  exit 2
+fi
 # Testing
 npm run test || exit $?
 # Commit code
 git add .
-git commit -m "Task 2 - part 2 completed and tested"
+git commit -m "Task 2 - part 2 completed and tested - uncommented code"
 git push origin master
 git status
